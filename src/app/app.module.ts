604565bd206212fac39d334ca2b5d4e1385dd563
daniele_bertoldi <daniele.bertoldi@itj.it>
2023-06-22 17:40:46 +0200

    Inizio progetto, aggiunto header, home e Quill

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './features/header/header.component';
+import { HomeComponent } from './features/home/home.component';
+import { QuillModule } from 'ngx-quill';
+import { RitchTextComponent } from './features/ritch-text/ritch-text.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    RitchTextComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    QuillModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/features/ritch-text/ritch-text.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ritch-text',
+  template: `
+    <p>
+      ritch-text works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class RitchTextComponent {
+
+}
