7edbaaf0249c29999e36dd73f64021bf24c866ae
daniele_bertoldi <daniele.bertoldi@itj.it>
2023-06-26 17:57:09 +0200

    Fix headers per login e registrazione in userService

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -15,10 +15,18 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   register(utente: RegistrazioneDTO): Observable<UtenteOutput> {
-    return this.http.post<UtenteOutput>(enviroment.endpoint + '/users/signIn', utente);
+    return this.http.post<UtenteOutput>(enviroment.endpoint + '/users/signIn', utente, {
+      headers: {
+        skip: "true"
+      }
+    });
   }
 
   login(utente: LoginDTO): Observable<Autenticazione> {
-    return this.http.post<Autenticazione>(enviroment.endpoint + '/users/login', utente);
+    return this.http.post<Autenticazione>(enviroment.endpoint + '/users/login', utente, {
+      headers: {
+        skip: "true"
+      }
+    });
   }
 }
