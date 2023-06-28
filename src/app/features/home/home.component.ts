840afc92da627b53b66bf1b79f6efc81f7c177d8
daniele_bertoldi <daniele.bertoldi@itj.it>
2023-06-28 15:36:13 +0200

    Aggiunta visualizzazione e formattazione immagine in Mat-card-image

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { enviroment } from 'src/app/shared/enviroment';
+import { JSONToHTML, JSONType } from 'html-to-json-parser'; 
 import HTMLParser from 'html-to-json-parser';
 
 @Component({
@@ -13,7 +14,9 @@ export class HomeComponent {
   user: string = enviroment.name!;
   isTextVisible: boolean = false;
   formValues?: FormGroup;
-  jsonP?: any;
+  jsonParsed?: any;
+  imgVal?: any;
+  imgHtml?: any;
 
   constructor(private router: Router) { }
 
@@ -24,12 +27,21 @@ export class HomeComponent {
   private async jsonConv() {
     const res = await HTMLParser(this.formValues!.value.corpo, false);
     console.log("CONV", res);
+    this.jsonParsed = res;
+  }
+
+  private async imgHtmlConv() {
+    console.log("CORPO JSON", this.jsonParsed);
+    this.imgVal = this.jsonParsed.content.find((element: any) => element.type === 'img');
+    console.log("IMG ATTRIBUTES", this.imgVal);
+    this.imgHtml = await JSONToHTML(this.imgVal, true);
   }
 
   async catchData(event: FormGroup) {
     console.log("METODO CATCH: ", event);
     this.formValues = event;
     await this.jsonConv();
+    await this.imgHtmlConv();
   }
 
 
