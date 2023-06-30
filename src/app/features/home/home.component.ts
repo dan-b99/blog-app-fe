7d6718aeb5dbfb0e23a9db16abca9767a83e28e6
daniele_bertoldi <daniele.bertoldi@itj.it>
2023-06-30 12:14:40 +0200

    Aggiunta visualizzazione articolo con stringhe

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { enviroment } from 'src/app/shared/enviroment';
-import parse from 'html-dom-parser';
 
 @Component({
   selector: 'app-home',
@@ -13,15 +12,11 @@ export class HomeComponent {
   user: string = enviroment.name!;
   isTextVisible: boolean = false;
   formValues?: FormGroup;
-  jsonParsed?: any;
-  imgVal?: any;
-  contentBody?: any;
-  @ViewChild('imgTest') imgEl?: ElementRef;
-  @ViewChild('testo') textEl?: ElementRef;
-  @ViewChild('title') titleTest?: ElementRef;
+  regex = /<img[^>]*>/;
+  imgHTML?: string;
+  bodyHTML?: string;
 
   constructor(private router: Router) { }
-
   
   toggle() {
     this.isTextVisible = !this.isTextVisible;
@@ -30,39 +25,18 @@ export class HomeComponent {
   catchData(event: FormGroup) {
     console.log("METODO CATCH: ", event);
     this.formValues = event;
-    console.log("TITOLO HTML", this.titleTest?.nativeElement);
-    this.jsonConv();
     this.imgSearch();
     this.bodyAssignment();
-    this.settingHTML();
-  }
-  
-  private jsonConv() {
-    this.jsonParsed = parse(this.formValues!.value.corpo);
-    console.log("PARSED", this.jsonParsed);
   }
   
   private imgSearch() {
-    this.jsonParsed.map(
-      (singleObj: any) => {
-        if(singleObj.children.find((element: any) => element.name === 'img')) {
-          this.imgVal = singleObj;
-        }
-      }
-      );
-      console.log("CERCO IMMAGINE", this.imgVal);
+    const matchings = this.regex.exec(this.formValues?.value.corpo);
+    if(matchings) {
+      this.imgHTML = matchings[0];
     }
+  }
     
     private bodyAssignment() {
-      //TODO: for con indice su element.children per evitare di hardcodare l'indice (o while idx<element.children.length)
-      this.contentBody = this.jsonParsed.filter((element: any) => element.children[0].name !== 'img');
-      this.contentBody.map((single: any) => {
-        
-      });
-    }
-
-    private settingHTML() {
-      const htmlImg = this.imgEl?.nativeElement;
-      console.log("HTML IMG", htmlImg);
+      this.bodyHTML = this.imgHTML ? this.formValues?.value.corpo.replace(this.regex, '') : this.formValues?.value.corpo;
     }
   }
